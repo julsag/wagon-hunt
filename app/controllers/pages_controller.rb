bd8bd92f6fbfbab42d12c226ae002a09873fd152
julsag <sagotjul@gmail.com>
2016-11-23 16:11:50 +0100

    db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
-  def home
-  end
+	def home 
+	  	@today = Date.today
+	end
+	def team
+		@members = ["jul", "toine", "helene"]
+	end
+	def join_us
+		render plain:"join me"
+	end
 end
